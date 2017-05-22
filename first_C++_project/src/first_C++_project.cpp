28e3084c84eae14d76b0669f62b9456151a911f7
migpir <tigerito123@yahoo.com>
2017-05-22 14:39:01 -0400

    Version 1.0:  printing to standard output using std::cout

---
+++ first_C++_project/src/first_C++_project.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : first_C++_project.cpp
+// Author      : Miguel H. Peralta
+// Version     :
+// Copyright   : Your copyright notice
+// Description : C++ practice tutorial
+//             : version 1.0  how to print on stdout using std::cout
+//             :  print "Hello World" to standard output
+//============================================================================
+
+#include <iostream>
+
+
+int main() {
+	std::cout << "!!!Hello World 1!!!" << std::endl; // prints !!!Hello World!!!
+	return 0;
+}
