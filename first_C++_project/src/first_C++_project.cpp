b9b10677b6eaf4447aa8497044b527ca64b941c8
migpir <tigerito123@yahoo.com>
2017-05-22 15:00:31 -0400

    SCR # 1.1 added six data type variables with printout.

--- first_C++_project/src/first_C++_project.cpp
+++ first_C++_project/src/first_C++_project.cpp
@@ -5,13 +5,31 @@
 // Copyright   : Your copyright notice
 // Description : C++ practice tutorial
 //             : version 1.0  how to print on stdout using std::cout
-//             :  print "Hello World" to standard output
+//             : 1.0 print "Hello World" to standard output
+//             : 1.1 remove std:: by adding namespace
+//             : 1.2 Define and initialize 6 data types and output to screen
 //============================================================================
 
 #include <iostream>
-
+#include <vector>
+#include <fstream>
+using namespace std;
 
 int main() {
-	std::cout << "!!!Hello World 1!!!" << std::endl; // prints !!!Hello World!!!
+
+	const double PI = 3.1428576;
+	int myAge = 39;
+	char myGrade = 'A';
+	bool isHappy = true;
+	float Num = 2.1418281828459045;
+	double  favnum = 3.245678;
+
+	cout<<"PI is equal to "<<PI<<endl;
+	cout<<"myAge is  "<<myAge<<endl;
+	cout<<"myGrade is equal to "<<myGrade<<endl;
+	cout<<"Am I happy? "<<isHappy<<endl;
+	cout<<"Num is equal to "<<Num<<endl;
+	cout<<"favnum is equal to "<<favnum<<endl;
+
 	return 0;
 }
